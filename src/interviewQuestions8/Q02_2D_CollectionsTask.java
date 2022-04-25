b2dcc950c0a5be44e91cbc5aa436d9a6cf6d3256
zulem26 <pruvanetastore@gmail.com>
2022-04-25 18:28:09 +0300

    25.04.2022

---
+++ src/interviewQuestions8/Q02_2D_CollectionsTask.java
@@ -0,0 +1,11 @@
+package interviewQuestions8;
+
+public class Q02_2D_CollectionsTask {
+    // Create 2D ArrayList which can store String ArrayLists
+    // Create 3 ArrayLists which are Employees, Employers, Companies
+    // Store this 3 ArrayList in 2D ArrayList
+
+    // String ArrayList'leri depolayabilen 2D ArrayList oluşturun
+    // Çalışanlar, İşverenler, Şirketler olmak üzere 3 DiziListesi oluşturun
+    // Bu 3 ArrayList'i 2D ArrayList'te saklayın
+}
